8c615b96c9f82ef3f0f1cea3a7cc91c1a7e9e29a
Ehan <admin@admins-iMac.local>
2025-02-23 21:16:04 +0000

    level 5 completed

--- src/components/Character/Character.jsx
+++ src/components/Character/Character.jsx
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from "react";
 import "./Character.css";
 
-
-const RunningCharacter = () => {
+const RunningCharacter = ({ setIsPlaying }) => {
   const [frame, setFrame] = useState(0);
+  const [isVisible, setIsVisible] = useState(true);
+  const [isFading, setIsFading] = useState(false);
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -13,10 +14,50 @@ const RunningCharacter = () => {
     return () => clearInterval(interval);
   }, []);
 
+  useEffect(() => {
+    const checkCollision = () => {
+      const movingObject = document.querySelector(".moving-object");
+      const helicopter = document.querySelector(".helicopter");
+
+      if (movingObject && helicopter) {
+        const movingRect = movingObject.getBoundingClientRect();
+        const helicopterRect = helicopter.getBoundingClientRect();
+
+        if (
+          movingRect.right >= helicopterRect.left &&
+          movingRect.left <= helicopterRect.right &&
+          movingRect.bottom >= helicopterRect.top &&
+          movingRect.top <= helicopterRect.bottom
+        ) {
+          setIsPlaying(false);
+          setIsFading(true);
+          setTimeout(() => setIsVisible(false), 500); // Fade-out duration
+        }
+      }
+    };
+
+    const collisionInterval = setInterval(checkCollision, 100);
+
+    return () => clearInterval(collisionInterval);
+  }, [setIsPlaying]);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setIsFading(true);
+      setTimeout(() => setIsVisible(false), 500); // Fade-out duration
+    }, 60000); // 1 minute
+
+    return () => clearTimeout(timeout);
+  }, []);
+
   return (
     <div className="animation-container">
-      <div className="moving-object" />
-      <img src='https://media2.giphy.com/media/qPnlSzSYg5QkWwbTOv/giphy.gif?cid=6c09b952ly19d9pj8sq4732kjypq697trwfh7qb4r5q3uhup&ep=v1_stickers_search&rid=giphy.gif&ct=shttps://i.pinimg.com/originals/be/61/4d/be614d780f0cd3b930fd13f3efb8689e.gif' alt="Helicopter" className="helicopter" />
+      {isVisible && (
+        <>
+          <div className={`moving-object ${isFading ? 'fade-out' : ''}`} />
+          <img src='https://media2.giphy.com/media/qPnlSzSYg5QkWwbTOv/giphy.gif?cid=6c09b952ly19d9pj8sq4732kjypq697trwfh7qb4r5q3uhup&ep=v1_stickers_search&rid=giphy.gif&ct=shttps://i.pinimg.com/originals/be/61/4d/be614d780f0cd3b930fd13f3efb8689e.gif' alt="Helicopter" className={`helicopter ${isFading ? 'fade-out' : ''}`} />
+        </>
+      )}
     </div>
   );
 };
